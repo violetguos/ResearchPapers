f293851bb8497e6403ab57dbfdde0acabc8c5539
scheeloong <scheeloong@gmail.com>
2018-01-01 16:34:08 -0500

    .

---
+++ generateTableOfContents.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/ekalinin/github-markdown-toc/master/gh-md-toc
+chmod a+x gh-md-toc
+./gh-md-toc README.md > tmp.txt
+
+# Manually re-copy paste the table of contents into the .md file yourself
